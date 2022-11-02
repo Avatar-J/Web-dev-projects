ca6f86aefd1f3c7352fc08d5f518f6be07f0ce01
king jummai <jummai@tiuelabb6.ti.lab>
2022-11-02 19:13:37 +0000

    admin dashboard

---
+++ simple-admin-dashboard/src/AdminDashboard/links_and_icons.js
@@ -0,0 +1,20 @@
+import {IoLibraryOutline} from "react-icons/io"
+import React from "react"
+
+export const links_and_icons = [
+    {icon: <IoLibraryOutline/>,
+     link: "Virtual Library"   
+     },
+    // {
+    //     icon: <RiFolderUploadLine/>,
+    //     link: "upload file"
+    // },
+    // {
+    //     icon: <RiDeleteBin6Line/>,
+    //     link: "Delete file"
+    // },
+    // {
+    //     icon: <IoIosLogOut/>,
+    //     link: "LogOut"
+    // },
+]
---
+++ simple-admin-dashboard/src/App.js
@@ -0,0 +1,12 @@
+import Admin from "./AdminDashboard/Admin"
+
+
+function App() {
+  return (
+    <div className="App">
+     <Admin/>
+    </div>
+  );
+}
+
+export default App;
