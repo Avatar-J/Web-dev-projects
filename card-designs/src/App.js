c11400ccd4c7eecc6a096850f0de4650782281ee
Avatar-J <109317531+Avatar-J@users.noreply.github.com>
2022-11-09 19:36:45 +0000

    conflict resolved

--- card-designs/src/App.js
+++ card-designs/src/App.js
@@ -1,17 +1,11 @@
 import BottomCurvedCard from "./Components/Bottom-curved-card/Card";
-<<<<<<< HEAD
 import ProfileCard from "./Components/Profile-Card/ProfileCard";
-=======
->>>>>>> dc7fd57d55fe66a2bb312d17f7030203b61250bd
 
 function App() {
   return (
     <div className="App">
       <BottomCurvedCard/>
-<<<<<<< HEAD
       <ProfileCard/>
-=======
->>>>>>> dc7fd57d55fe66a2bb312d17f7030203b61250bd
     </div>
   );
 }
