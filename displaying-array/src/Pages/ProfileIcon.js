3be58b052b46dbc5f519149bed4418cf9c753a28
Avatar-J <109317531+Avatar-J@users.noreply.github.com>
2022-11-29 16:38:13 +0000

    testing

--- displaying-array/src/Pages/ProfileIcon.js
+++ displaying-array/src/Pages/ProfileIcon.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Female from "../Images/female.png"
-import Male from "../Images/man1.png"
+import Female from "../Images/female.png";
+import Male from "../Images/man1.png";
 import { CardLeft, Image } from "./Styling";
 
 export const ProfileIcon = (props)=>{
